291624ae80bd8ced51ba14af5e6f6a2ae93a2376
nsalinas350 <nsalinas@unal.edu.co>
2021-11-30 13:54:27 -0500

    Update Pais.java

--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -5,16 +5,20 @@ import java.util.ArrayList;
 public class Pais {
 	
 	private String nombre;
-	private ArrayList<Vehiculo>listado = new ArrayList<Vehiculo>();
+	public int vendidos;
+	private static  ArrayList<Pais>paises = new ArrayList<Pais>();
 	
 	public Pais(String nombre){
 		setNombre(nombre);
+		Pais.getPaises().add(this);
 	}
 	
 	public String getNombre() {return this.nombre;}
 	void setNombre(String nombre) {this.nombre = nombre;}
 	
-	public ArrayList<Vehiculo> getListado() {return this.listado;}
+	public int getVendidos() {return this.vendidos;}
+	
+	public static ArrayList<Pais> getPaises() {return Pais.paises;}
 	
 	public static Pais paisMasVendedor(){
 		return new Pais("Pais Vendedor");
