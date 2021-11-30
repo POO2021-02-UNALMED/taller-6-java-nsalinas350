8626e672cb47963e8bb4a8791e1f7daa577e205b
nsalinas350 <nsalinas@unal.edu.co>
2021-11-30 13:48:16 -0500

    Update Fabricante.java

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -1,16 +1,19 @@
 package vehiculos;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Fabricante {
 	
 	private String nombre;
 	private Pais pais;
-	private ArrayList<Vehiculo>listado = new ArrayList<Vehiculo>();
+	public int vendidos;
+	private static ArrayList<Fabricante>fabricantes = new ArrayList<Fabricante>();
 	
 	public Fabricante(String nombre, Pais pais){
 		setNombre(nombre);
 		setPais(pais);
+		Fabricante.getFabricantes().add(this);
 	}
 	
 	public String getNombre() {return this.nombre;}
@@ -19,10 +22,26 @@ public class Fabricante {
 	public Pais getPais() {return this.pais;}
 	void setPais(Pais pais) {this.pais = pais;}
 	
-	public ArrayList<Vehiculo> getListado() {return this.listado;}
+	public int getVendidos() {return this.vendidos;}
 	
-	public static Fabricante fabricaMayorVentas(){
-		return new Fabricante("Empresa vendedora",new Pais("Pais Generico"));
+	public static ArrayList<Fabricante> getFabricantes() {return Fabricante.fabricantes;}
+	
+	public static Object fabricaMayorVentas(){
+		ArrayList<Integer> listaVendidos = new ArrayList<Integer>();
+		Fabricante maxVendedor = null;
+		
+		for (Fabricante f: fabricantes) {
+			listaVendidos.add(f.getVendidos());
+		}
+		
+		int maximoVendidos = Collections.max(listaVendidos);
+		for (Fabricante f: fabricantes) {
+			if (f.getVendidos() == maximoVendidos) {
+				maxVendedor = f;
+			}
+		}
+		
+		return maxVendedor;
 	}
 
 
